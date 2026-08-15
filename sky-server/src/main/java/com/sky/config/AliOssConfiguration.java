64e40bd0466c545cd2286b02763bea7b1e7f13f5
xcx <1409551707@qq.com>
2026-08-15 19:18:35 +0800

    商品浏览用户端业务代码

--- sky-server/src/main/java/com/sky/config/AliOssConfiguration.java
+++ sky-server/src/main/java/com/sky/config/AliOssConfiguration.java
@@ -17,7 +17,7 @@ public class AliOssConfiguration {
     @Bean
     @ConditionalOnMissingBean
     public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
-        log.info("开始创建阿里云文件上传工具类对象：{}", aliOssProperties);
+        log.info("开始创建阿里云文件上传工具类对象：bucketName={}, endpoint={}", aliOssProperties.getBucketName(), aliOssProperties.getEndpoint());
         return new AliOssUtil(aliOssProperties.getEndpoint(),
                 aliOssProperties.getAccessKeyId(),
                 aliOssProperties.getAccessKeySecret(),
